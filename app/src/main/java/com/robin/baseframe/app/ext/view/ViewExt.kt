c22caa5afba9329e84b472ce4ccd2256e934a0b2
robinying <robin_ying@outlook.com>
2021-11-04 16:48:15 +0800

    add base recycleview adapter

--- app/src/main/java/com/robin/baseframe/app/ext/view/ViewExt.kt
+++ app/src/main/java/com/robin/baseframe/app/ext/view/ViewExt.kt
@@ -155,3 +155,24 @@ fun View.extraAnimClickListener(animator: ValueAnimator, action: (View) -> Unit)
 }
 
 
+/**
+ * Registers the [block] lambda as [View.OnClickListener] to this View.
+ *
+ * If this View is not clickable, it becomes clickable.
+ */
+inline fun View.onClick(block: View.OnClickListener) = setOnClickListener(block)
+
+/**
+ * Register the [block] lambda as [View.OnLongClickListener] to this View.
+ * By default, [consume] is set to true because it's the most common use case, but you can set it
+ * to false.
+ * If you want to return a value dynamically, use [View.setOnLongClickListener] instead.
+ *
+ * If this view is not long clickable, it becomes long clickable.
+ */
+inline fun View.onLongClick(
+    consume: Boolean = true,
+    crossinline block: () -> Unit
+) = setOnLongClickListener { block(); consume }
+
+
