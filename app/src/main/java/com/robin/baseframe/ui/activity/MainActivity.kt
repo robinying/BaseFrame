ff56b3f6e31ea85e4f6f9fa88723653832c081d7
robinying <robin_ying@outlook.com>
2021-11-08 15:15:32 +0800

    mainfragment

--- app/src/main/java/com/robin/baseframe/ui/activity/MainActivity.kt
+++ app/src/main/java/com/robin/baseframe/ui/activity/MainActivity.kt
@@ -3,10 +3,14 @@ package com.robin.baseframe.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.robin.baseframe.R
+import com.robin.baseframe.app.base.BaseActivity
+import com.robin.baseframe.app.base.BaseViewModel
+import com.robin.baseframe.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<BaseViewModel, ActivityMainBinding>() {
+
+
+    override fun initView(savedInstanceState: Bundle?) {
+
     }
 }
---
+++ app/src/main/java/com/robin/baseframe/ui/fragment/MainFragment.kt
@@ -0,0 +1,12 @@
+package com.robin.baseframe.ui.fragment
+
+import android.os.Bundle
+import com.robin.baseframe.app.base.BaseFragment
+import com.robin.baseframe.app.base.BaseViewModel
+import com.robin.baseframe.databinding.FragmentMainBinding
+
+class MainFragment:BaseFragment<BaseViewModel,FragmentMainBinding>() {
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
+}
