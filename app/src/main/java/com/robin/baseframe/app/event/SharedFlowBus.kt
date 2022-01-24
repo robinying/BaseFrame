3f5a0465d5f1206d30fd407e202151a02b2f6c3a
robinying <31560412+robinying@users.noreply.github.com>
2022-01-24 14:11:32 +0800

    add shared flow bus

---
+++ app/src/main/java/com/robin/baseframe/app/event/SharedFlowBus.kt
@@ -0,0 +1,39 @@
+package com.robin.baseframe.app.event
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import java.util.concurrent.ConcurrentHashMap
+
+/**
+ * SharedFlowBus 只会将更新通知给活跃的观察者，
+ */
+object SharedFlowBus {
+
+    private var events = ConcurrentHashMap<Any, MutableSharedFlow<Any>>()
+    private var stickyEvents = ConcurrentHashMap<Any, MutableSharedFlow<Any>>()
+
+    fun <T> with(objectKey: Class<T>): MutableSharedFlow<T> {
+        if (!events.containsKey(objectKey)) {
+            events[objectKey] = MutableSharedFlow(0, 1, BufferOverflow.DROP_OLDEST)
+        }
+        return events[objectKey] as MutableSharedFlow<T>
+    }
+
+    fun <T> withSticky(objectKey: Class<T>): MutableSharedFlow<T> {
+        if (!stickyEvents.containsKey(objectKey)) {
+            stickyEvents[objectKey] = MutableSharedFlow(1, 1, BufferOverflow.DROP_OLDEST)
+        }
+        return stickyEvents[objectKey] as MutableSharedFlow<T>
+    }
+
+    fun <T> on(objectKey: Class<T>): LiveData<T> {
+        return with(objectKey).asLiveData()
+    }
+
+    fun <T> onSticky(objectKey: Class<T>): LiveData<T> {
+        return withSticky(objectKey).asLiveData()
+    }
+
+}
