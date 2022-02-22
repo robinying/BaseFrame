9ec72572eed9d5cde1e5d1b07d9549fb783eaf6f
robinying <31560412+robinying@users.noreply.github.com>
2022-02-22 11:22:04 +0800

    add status bar demo

--- app/src/main/java/com/robin/baseframe/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/robin/baseframe/ui/fragment/MainFragment.kt
@@ -1,6 +1,13 @@
 package com.robin.baseframe.ui.fragment
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+import android.view.WindowInsets
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.view.ViewCompat
 import com.robin.baseframe.R
 import com.robin.baseframe.app.base.BaseFragment
 import com.robin.baseframe.app.base.BaseViewModel
@@ -34,5 +41,19 @@ class MainFragment : BaseFragment<BaseViewModel, FragmentMainBinding>() {
         data.forEach {
             LogUtils.debugInfo("$it")
         }
+        //沉浸式状态栏
+        //Android 11开始提供新的API WindowInsetsController
+        binding.mainCl.systemUiVisibility = (SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        /*
+        * 这里其实可以借助setOnApplyWindowInsetsListener()函数去监听WindowInsets发生变化的事件，
+        * 当有监听到发生变化时，我们可以读取顶部Insets的大小，然后对控件进行相应距离的偏移。
+        * */
+        ViewCompat.setOnApplyWindowInsetsListener(binding.toolBar) { view, insets ->
+            val params = view.layoutParams as ConstraintLayout.LayoutParams
+            params.topMargin = insets.systemWindowInsetTop
+            insets
+        }
+
     }
 }
