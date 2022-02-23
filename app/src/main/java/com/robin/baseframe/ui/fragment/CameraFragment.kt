074c6f7b7e32ca04f4a0db6dfafa0cbdebbe4b73
robinying <31560412+robinying@users.noreply.github.com>
2022-02-23 15:51:21 +0800

    camera preview

---
+++ app/src/main/java/com/robin/baseframe/ui/fragment/CameraFragment.kt
@@ -0,0 +1,88 @@
+package com.robin.baseframe.ui.fragment
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.robin.baseframe.app.base.BaseFragment
+import com.robin.baseframe.app.base.BaseViewModel
+import com.robin.baseframe.app.ext.nav
+import com.robin.baseframe.databinding.FragmentCameraBinding
+import jp.co.cyberagent.android.gpuimage.GPUImage
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class CameraFragment:BaseFragment<BaseViewModel,FragmentCameraBinding>() {
+    private lateinit var cameraExecutor: ExecutorService
+    private var bitmap: Bitmap? = null
+    private var gpuImage: GPUImage? = null
+    private var isTakePhoto = false
+    override fun initView(savedInstanceState: Bundle?) {
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                mActivity, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(requireContext(),
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                nav().navigateUp()
+            }
+        }
+    }
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+    }
+}
